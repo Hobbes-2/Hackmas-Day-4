51bc4112773a987b013717e6156c1916e0be2995
Hobbes-2 <kiko@arch.localdomain>
2025-12-16 23:47:22 -0500

    a

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,17 +2,9 @@ import "dotenv/config"
 import { drizzle } from "drizzle-orm/bun-sqlite"
 
 const { exec } = require("child_process")
-
 exec("bunx drizzle-kit push", (error, stdout, stderr) => {
     if (error) {
-        console.log("Error:", error.message)
-        return
+        console.log("Error:", error.message);
     }
-    if (stderr) {
-        console.log("Stderr:", stderr)
-        return
-    }
-    console.log("Output:",
-                stdout)
-})
+});
 export const db = drizzle(process.env.DB_FILE_NAME)
